432ba5d610fbcd742d7fc883ee670efaadf88fef
Siddhesh Nikude <Siddhesh.Nikude@nelkinda.com>
2024-01-15 14:33:26 +0530

    Implement failing Acceptance test.

--- cypress/e2e/features/todo.feature
+++ cypress/e2e/features/todo.feature
@@ -4,3 +4,9 @@ Feature: Todo App functionality
     Given I visit the Todo Application
     When I add "Send email" as Todo item
     Then I should see "Send email" in the Todo list
+
+  Scenario: Edit a Todo
+    Given I have a Todo item "Send email"
+    When I edit "Send email" to "Write blog"
+    Then I should see "Write blog" in the Todo list
+    And I should not see "Send email" in the Todo list
--- cypress/e2e/features/todo/todo.ts
+++ cypress/e2e/features/todo/todo.ts
@@ -12,6 +12,21 @@ Then('I should see {string} in the Todo list', (text: string) => {
     cy.get('ul').should('contain', text);
 });
 
+Given('I have a Todo item {string}', (text: string) => {
+    visitLink()
+    addTodo(text)
+});
+
+When('I edit {string} to {string}', (oldText: string, newText: string) => {
+    cy.contains('li', oldText).click()
+    editTodo(newText);
+});
+
+Then('I should not see {string} in the Todo list', (text: string) => {
+    !cy.get('ul').should('contain', text);
+
+});
+
 function visitLink() {
     cy.visit('http://localhost:8080/');
 }
@@ -19,4 +34,9 @@ function visitLink() {
 function addTodo(text: string) {
     cy.get('input[placeholder="Enter a todo"]').type(text);
     cy.get('button').contains('Add Todo').click();
+}
+
+function editTodo(newText: string) {
+    cy.get('input[placeholder="Enter a todo"]').type(newText);
+    cy.get('button').contains('Edit Todo').click();
 }
